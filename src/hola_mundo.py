0810576a82c153800220f68ef788ea2bf7ca9fdf
lastvyandy <73372589+lastvyandy@users.noreply.github.com>
2023-05-29 13:03:38 -0400

    Update hola_mundo.py

--- src/hola_mundo.py
+++ src/hola_mundo.py
@@ -2,5 +2,5 @@ import structlog
 
 logger = structlog.get_logger(__file__)
 
-logger.info("test", contexto="Contexto de pruebas.........")
+logger.info("test", contexto="Contexto de pruebass.........")
 
