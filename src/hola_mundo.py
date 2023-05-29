11892dcdcd8baf1adc24806c46649cc64c52e9b8
lastvyandy <jorgemanriquezcnt@gmail.com>
2023-05-29 15:18:08 -0400

    - general:   - add hat wobble ^--^  ^------------^ |     | |     +-> Summary in present tense. | +-------> Type: docs

--- src/hola_mundo.py
+++ src/hola_mundo.py
@@ -2,5 +2,5 @@ import structlog
 
 logger = structlog.get_logger(__file__)
 
-logger.info("tesdt", contexto="Contexto de pruebass....cc.ddd.ssdsdsdsdssdsds.dd. yo continuo yo prosigo ")
+logger.info("tesdt", contexto="Contexto de pruebass....cc.ddd.ssdsdsdsdssdsds.dd. yo csdsdsontinuo yo prosigo ")
 
